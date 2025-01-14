2b54e286a3d708738e6738ef53e6fd8c860d7ac4
lyDiyamong <diyamong26@gmail.com>
2025-01-14 16:58:14 +0700

    fix : auth layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,33 @@
 <template>
     <main class="auth-layout">
-        <NuxtImg src="/images/logo.png" width="45" alt="Logo img" />
-        <NuxtPage />
+        <section class="center-container">
+            <NuxtImg src="/images/logo.png" width="45" alt="Logo img" />
+            <NuxtPage />
+        </section>
     </main>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style scoped>
-.auth-layout {
-    max-width: 500px;
-    width: 100%;
-    margin: 150px auto;
-    padding: 42px;
-    background-color: white;
-    border-radius: var(--border-radius-section);
-}
+    .auth-layout {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        width: 100%;
+    }
 
+    .center-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        max-width: 500px;
+        margin: 0 auto;
+        padding: 42px;
+        border-radius: var(--border-radius-section);
+        background-color: white;
+    }
 </style>
