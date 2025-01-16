41cd9dbaaae9793880d9a4cf7647b40f01c5fe0f
lyDiyamong <diyamong26@gmail.com>
2025-01-16 12:52:05 +0700

    add : button on save for upload image title

--- components/EditResume/UploadImage.vue
+++ components/EditResume/UploadImage.vue
@@ -1,21 +1,25 @@
 <template>
-    <a-upload
-        v-model:file-list="fileList"
-        name="avatar"
-        list-type="picture-card"
-        class="avatar-uploader"
-        :show-upload-list="false"
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        :before-upload="beforeUpload"
-        @change="handleChange"
-    >
-        <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
-        <div v-else>
-            <loading-outlined v-if="loading" />
-            <plus-outlined v-else />
-            <div class="ant-upload-text">Upload</div>
-        </div>
-    </a-upload>
+    <div class="flex-between" style="margin-bottom: 16px;">
+        <a-upload
+            v-model:file-list="fileList"
+            name="avatar"
+            list-type="picture-card"
+            class="avatar-uploader"
+            :show-upload-list="false"
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            :before-upload="beforeUpload"
+            @change="handleChange"
+        >
+            <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+            <div v-else>
+                <loading-outlined v-if="loading" />
+                <plus-outlined v-else />
+                <div class="ant-upload-text">Upload</div>
+            </div>
+        </a-upload>
+
+        <a-button style="align-self: flex-end;" type="primary" >Save</a-button>
+    </div>
 </template>
 
 <script lang="ts" setup>
